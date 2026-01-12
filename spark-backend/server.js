f4cad6790b6260fe7be26be01f3639eff6984ee7
sekotonjabulo-rgb <sekotonjabulo@gmail.com>
2026-01-12 14:01:30 +0200

    Update server.js

--- spark-backend/server.js
+++ spark-backend/server.js
@@ -21,18 +21,7 @@ if (missingVars.length > 0) {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: [
-        'https://sekotonjabulo-rgb.github.io',
-        'https://lethabo3.github.io',
-        'http://localhost:3000',
-        'http://localhost:5500',
-        'http://127.0.0.1:5500',
-        'http://sparkadate.online',
-        'https://sparkadate.online'
-    ],
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
