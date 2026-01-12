0bd2d2598899c54e7245235f32046ef13289e5e5
sekotonjabulo-rgb <sekotonjabulo@gmail.com>
2026-01-12 12:51:46 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://sparkadate-ggbxuq.fly.dev/api';
 function getToken() {
     return localStorage.getItem('sparkToken');
 }
